a32e17586b79e09e261cff722f855e2d6bd4eba1
mavlonergashev <mavlik.exe@gmail.com>
2022-04-15 15:50:31 +0500

    Initial Commit

---
+++ hw core data to do list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw core data to do list
+//
+//  Created by Mavlon on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
